934ed1b0e0c901ed98d238e9971034dcf3413b38
Lorcan1 <lorcan.rooney@ucdconnect.ie>
2023-05-11 19:03:18 +0100

    Reads stats from backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+  
+
 
 @Component({
   selector: 'app-root',
@@ -6,4 +9,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-}
+  title = 'image-gallery';
+  public data:any = []
+  constructor(private http: HttpClient) {
+    
+  }
+ 
+  getData(){
+    const url ='http://localhost:8080/get-player'
+    this.http.get(url).subscribe((res)=>{
+      this.data = res
+      console.log(this.data)
+    })
+  }
+ }
